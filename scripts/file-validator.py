60619408a4a7c2c695a118958a6a16f2b773d68f
emilianolel <eherrera1331@gmail.com>
2024-01-09 17:30:28 -0600

    Error handling and validation function

--- scripts/file-validator.py
+++ scripts/file-validator.py
@@ -1,7 +1,8 @@
 import os
-from typing import Text, List
+from typing import Text, List, Tuple, Dict
 import yaml
 import csv
+import pandas as pd
 
 
 class FileValidator:
@@ -9,41 +10,44 @@ class FileValidator:
 
     def __init__(self, data_file, metadata_file):
 
-        self.data_file = exit(1) if not FileValidator._data_file_exists(data_file) else data_file
+        if not self._data_file_exists(data_file):
+            raise FileNotFoundError(f'Error: File {data_file} does not exist.')
+        
+        self.data_file = data_file
         self.metadata_file = metadata_file
-        
-        self.filename, self.file_extension, self.separator, self.num_columns, self.columns = self._get_metadata_info()
+
+        self.filename, self.file_extension, self.separator, self.num_columns, self.columns, self.not_null_cols = self._get_metadata_info()
 
 
     @staticmethod
-    def _data_file_exists(data_file):
-        
-        if not os.path.isfile(data_file):
-            print(f'Error: File {data_file} does not exist.')
-            exit(1)
-        return True
+    def _data_file_exists(data_file: Text) -> bool:
+
+        return os.path.isfile(data_file)
 
 
     @staticmethod
-    def _get_column_names(meta_dict):
+    def _get_column_names(meta_dict: Dict) -> List:
 
         return [column.get('name') for column in meta_dict]
 
 
-    def _get_metadata_info(self):
+    def _get_metadata_info(self) -> Tuple[Text, Text, Text, int, List, List]:
 
         with open(self.metadata_file, 'r') as stream:
             try:
                 metadata_dict = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
-                print(exc)
-                exit(1)
+                raise ValueError(f'Metadata Error: {exc}')
 
         meta_dict = metadata_dict['metadata']
+
+        if not meta_dict:
+            raise ValueError('Metadata Error: Metadata not found in the YAML file.')
 
         meta_file_dict = meta_dict.get('file')
         meta_struct_dict = meta_dict.get('structure')
         meta_col_dict = meta_dict.get('columns')
+        meta_not_null_dict = meta_dict.get('not_null')
 
         filename = meta_file_dict.get('filename')
         file_extension = meta_file_dict.get('extension')
@@ -51,31 +55,61 @@ class FileValidator:
         number_of_columns = meta_struct_dict.get('num_columns')
 
         columns = FileValidator._get_column_names(meta_col_dict)
+        not_null_cols = FileValidator._get_column_names(meta_not_null_dict)
 
-        return filename, file_extension, separator, number_of_columns, columns
+        return filename, file_extension, separator, number_of_columns, columns, not_null_cols
 
 
-    def _header_validation(self):
+    def header_validation(self) -> bool:
+
         with open(self.data_file) as file:
-            reader = csv.reader(file, delimiter='|')
+            reader = csv.reader(file, delimiter=self.separator)
             header = next(reader)
 
-        normalized_header = [column.lower() for column in header]
+        normalized_header = [column.upper() for column in header]
 
         if len(normalized_header) != self.num_columns:
-            print(f'File Format Error: File has {len(normalized_header)} columns and must be {self.num_columns}.')
-            return False
-
+            raise ValueError(f'File Format Error: File has {len(normalized_header)} columns and must be {self.num_columns}.')
+        
         if not set(normalized_header) <= set(self.columns):
             invalid_columns = set(normalized_header) - set(self.columns)
-            print(f'File Format Error: Invalid column name(s): {invalid_columns}. Possible column names: {self.columns}')
-            return False
-
-        if not normalized_header == self.columns:
-            print(f'File Format Error: Wrong column order. It must be {self.columns}')
-            return False
+            raise ValueError(f'File Format Error: Invalid column name(s): {invalid_columns}. Possible column names: {self.columns}')
+        
+        if normalized_header != self.columns:
+            raise ValueError(f'File Format Error: Wrong column order. It must be {self.columns}')
 
         return True
+
+    
+    @staticmethod
+    def _not_null_column(df: pd.DataFrame, col: Text) -> bool:
+
+        if any(df[col].isnull()):
+            raise ValueError(f'Not Null Error: There are null values in column {col}.')
+
+        return True
+
+    
+    def not_null_validation(self) -> bool:
+        
+        df = pd.read_csv(self.data_file, sep=self.separator)
+
+        for column in self.not_null_cols:
+            if not FileValidator._not_null_column(df, column):
+                return False
+
+        return True
+
+
+    def validation(self) -> None:
+        
+        if not self.header_validation():
+            raise ValueError('Header error')
+
+        if not self.not_null_validation():
+            raise ValueError('Not null column error')
+
+        print('Validación Exitosa!')
 
 
 if __name__ == "__main__":
@@ -84,10 +118,8 @@ if __name__ == "__main__":
     path = '../files/survey_lung_cancer.csv'
     metadata = '../metadata/survey_lung_cancer_metadata.yaml'
 
+    df = pd.read_csv(path, sep='|')
+
     file_val = FileValidator(path, metadata)
 
-    metadata_dict = file_val._get_metadata_info()
-
-    print(metadata_dict)
-
-    print(file_val._header_validation())
+    file_val.validation()
